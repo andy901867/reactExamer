f6a00bcd9ced4ae89addd738c21a8980e6f49d3e
Andy <andy901867@gmail.com>
2025-09-23 14:40:13 +0800

    題目資料改回public、但是特別去處理public路徑

--- src/lib/GetExamList.ts
+++ src/lib/GetExamList.ts
@@ -1,24 +1,26 @@
 import { type ExamOption } from "../components/modals/ExamSelectionModal";
 
 function toId(p: string) {
-  return p.split('/questionData/')[1].replace(/\.json$/,'');
+  return p.split('/questionData/')[1].replace(/\.json$/, '');
 }
 
 function getName(p: string) {
   // 取最後一段檔名當顯示用名稱，順便美化
-  const file = p.split('/').pop()!.replace(/\.json$/,'');
+  const file = p.split('/').pop()!.replace(/\.json$/, '');
   return decodeURIComponent(file)
     .replace(/[_-]+/g, ' ')
     .replace(/\b\w/g, s => s.toUpperCase());
 }
 
-const exams = import.meta.glob('../../questionData/*.json');
+const exams = import.meta.glob('../../public/questionData/*.json');
 
 
 export function GetExamList(): ExamOption[] {
-  return Object.entries(exams).map(([url]) => ({
-    id: toId(url),
-    url,
-    label: getName(url),
-  }));
+  return Object.entries(exams).map(([url]) => {
+    return {
+      id: toId(url),
+      url: url.split("public")[1],
+      label: getName(url),
+    }
+  });
 }
